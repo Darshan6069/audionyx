77f98574cba009e4e2c5b51375e9ceffbc91602f
Darshan6069 <138776334+Darshan6069@users.noreply.github.com>
2025-04-30 15:55:53 +0530

    feat:delete_playlistservice

--- lib/repository/bloc/auth_bloc_cubit/login_bloc_cubit/login_bloc_cubit.dart
+++ lib/repository/bloc/auth_bloc_cubit/login_bloc_cubit/login_bloc_cubit.dart
@@ -1,10 +1,13 @@
 import 'package:audionyx/repository/bloc/auth_bloc_cubit/login_bloc_cubit/login_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 import '../../../service/auth_service/login_service.dart';
 
 class LoginBlocCubit extends Cubit<LoginState> {
   final LoginService loginService = LoginService();
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   LoginBlocCubit() : super(const LoginState.initial());
 
@@ -20,12 +23,20 @@ class LoginBlocCubit extends Cubit<LoginState> {
     emit(const LoginState.loading());
 
     try {
-      final userId = await loginService.loginUser(email: email, password: password);
       final token = await loginService.loginUser(
         email: email,
         password: password,
       );
-      emit(LoginState.success(token,userId));
+
+      final payload = JwtDecoder.decode(token);
+      final userId = payload['id']?.toString();
+
+      await _storage.write(key: 'jwt_token', value: token);
+      if (userId != null) {
+        await _storage.write(key: 'userId', value: userId);
+      }
+
+      emit(LoginState.success(token, userId ?? ''));
     } catch (e) {
       emit(LoginState.failure(e.toString()));
     }
@@ -33,6 +44,8 @@ class LoginBlocCubit extends Cubit<LoginState> {
 
   Future<void> logout() async {
     await loginService.logout();
+    await _storage.delete(key: 'jwt_token');
+    await _storage.delete(key: 'userId');
     emit(const LoginState.initial());
   }
 }
